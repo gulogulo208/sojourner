7d20bfdbfa9dc2579cdef97e4cb3159179ee4af3
Jackson <jacksonfarren@hotmail.com>
2023-01-30 19:13:38 -0800

    added signup and login page funcitonality

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,6 +1,8 @@
 // import React, { useState, useEffect } from 'react';
 // import ButtonAppBar from '../components/Navbar';
 // import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
+import Nav from '../components/Navbar';
 
 
 
@@ -8,6 +10,8 @@ const Landing = () => {
     console.log("I AM IN THE LANDING PAGE!")
     return (
         <div>
+            <Nav />
+            <Link to="/login">Login</Link>
         </div>
     )
 
---
+++ client/src/pages/Login.js
@@ -0,0 +1,166 @@
+import React, { useState } from 'react';
+import { LOG_IN } from '../utils/mutation';
+import { useMutation } from '@apollo/client';
+/* import { Link } from 'react-router-dom'; */
+import Auth from '../utils/auth';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import ConnectingAirportsIcon from '@mui/icons-material/ConnectingAirports';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Carousel from 'react-bootstrap/Carousel';
+
+function Login(props) {
+    const [formState, setFormState] = useState({ email: '', password: ''});
+    const [login, { error }] = useMutation(LOG_IN);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables: { email: formState.email, password: formState.password },
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="#">
+            Sojourner
+        </Link>
+        {' '}
+        {new Date().getFullYear()}
+        {'.'}
+        </Typography>
+    );
+    }
+
+    const theme = createTheme();
+
+    return (
+        <ThemeProvider theme={theme}>
+        <Grid container component="main" sx={{ height: '100vh' }}>
+            <CssBaseline />
+            {/* <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className='d-block w-100'
+                        src='https://source.unsplash.com/random'
+                        alt='First slide'
+                    />
+                </Carousel.Item>
+            </Carousel>
+            </div> */}
+            <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
+            sx={{
+                backgroundImage: 'url(https://source.unsplash.com/random)',
+                backgroundRepeat: 'no-repeat',
+                backgroundColor: (t) =>
+                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+            />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Box
+                sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                mt: '350px'
+                }}
+            >
+                {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}> */}
+                <ConnectingAirportsIcon fontSize='large'/>
+                {/* </Avatar> */}
+                <Typography component="h1" variant="h5">
+                Sign in
+                </Typography>
+                <Box component="form" noValidate onSubmit={handleFormSubmit} sx={{ mt: 1 }}>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    onChange={handleChange}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    onChange={handleChange}
+                />
+                <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Remember me"
+                />
+                {error ? (
+                    <Typography>Invalid credentials</Typography>
+                ) : null}
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Sign In
+                </Button>
+                <Grid container>
+                    <Grid item xs>
+                    <Link href="#" variant="body2">
+                        Forgot password?
+                    </Link>
+                    </Grid>
+                    <Grid item>
+                    <Link href="/signup" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                    </Link>
+                    </Grid>
+                </Grid>
+                <Copyright sx={{ mt: 5 }} />
+                </Box>
+            </Box>
+            </Grid>
+        </Grid>
+        </ThemeProvider>
+    );
+}
+
+export default Login;

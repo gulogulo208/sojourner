04026d64b2250b8b5e38e620acf3071bc568eaf5
JamesJPorter <porterjjames@gmail.com>
2023-02-07 20:24:33 -0800

    another attempt!

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const db = require("./config/connection");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 

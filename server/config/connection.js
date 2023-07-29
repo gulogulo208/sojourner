933b18185b24df65a01f4ed816efafce17458f96
JamesJPorter <porterjjames@gmail.com>
2023-07-29 14:32:28 -0700

    setup dotenv

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
+
+for (let i = 1; i <= 20; i++) {
+  console.log(process.env.MONGODB_URI)
+}
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,

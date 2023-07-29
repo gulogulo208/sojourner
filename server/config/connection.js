b78c19474cae8f285c72a8cc23f7b2355fac2270
JamesJPorter <porterjjames@gmail.com>
2023-07-29 14:15:07 -0700

    mongoDB Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sojourner_DB', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
